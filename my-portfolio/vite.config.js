8be4d9534842c0fa9878682c63372fc27f33803f
NoRoshanrksKrSingh <no-roshan@snsoftwares.com>
2025-09-10 23:27:28 +0530

    for deploy

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',        // Base public path (important for correct asset loading)
+  base: '/',
   build: {
-    outDir: 'dist',  // Output directory for build files (default is 'dist')
-    sourcemap: false // Optional: disable sourcemaps in production to keep it clean
-  },
-  server: {
-    open: true      // Optional: opens browser automatically in dev mode
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html')
+    }
   }
 })
